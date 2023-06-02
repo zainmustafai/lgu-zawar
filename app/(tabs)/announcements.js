5036e0cd0147a3dcc37728b6dd2b1c5a91089918
zainmustafai <zforzain2000@gmail.com>
2023-06-02 16:39:31 +0500

    first 2 screens

---
+++ app/(tabs)/announcements.js
@@ -0,0 +1,131 @@
+import { View, ScrollView, Text, StyleSheet, TouchableOpacity, Platform} from 'react-native'
+import React from 'react'
+import AnnouncementCard from '../../components/AnnouncementCard/AnnouncementCard'
+import { Picker } from '@react-native-picker/picker';
+
+// FAKE DATA ARRAY
+const announcementsArray = [
+    {
+        title: 'Quiz1',
+        description: "Lorem ipsum general default text whatever you like wtf wednesday thursday friday.Lorem ipsum general default text whatever you like wtf wednesday thursday friday.",
+        time: '3:00',
+        stars: 4,
+    },
+    {
+        title: 'Assignment',
+        description: "Lorem ipsum general default text whatever you like wtf wednesday thursday friday.Lorem ipsum general default text whatever you like wtf wednesday thursday friday.",
+        time: '3:00',
+        stars: 3,
+    },
+    {
+        title: 'Quiz #',
+        description: "Lorem ipsum general default text whatever you like wtf wednesday thursday friday.Lorem ipsum general default text whatever you like wtf wednesday thursday friday.",
+        time: '3:00',
+        stars: 4,
+    },
+    {
+        title: 'Quiz 2',
+        description: "Lorem ipsum general default text whatever you like wtf wednesday thursday friday.Lorem ipsum general default text whatever you like wtf wednesday thursday friday.",
+        time: '3:00',
+        stars: 2,
+    },
+    {
+        title: 'Quiz 3',
+        description: "Lorem ipsum general default text whatever you like wtf wednesday thursday friday.Lorem ipsum general default text whatever you like wtf wednesday thursday friday.",
+        time: '3:00',
+        stars: 4,
+    },
+    {
+        title: 'Quiz 4',
+        description: "Lorem ipsum general default text whatever you like wtf wednesday thursday friday.Lorem ipsum general default text whatever you like wtf wednesday thursday friday.",
+        time: '3:00',
+        stars: 3,
+    },
+    {
+        title: 'Quiz 5',
+        description: "Lorem ipsum general default text whatever you like wtf wednesday thursday friday.Lorem ipsum general default text whatever you like wtf wednesday thursday friday.",
+        time: '3:00',
+        stars: 4,
+    },
+    {
+        title: 'Quiz 6',
+        description: "Lorem ipsum general default text whatever you like wtf wednesday thursday friday.Lorem ipsum general default text whatever you like wtf wednesday thursday friday.",
+        time: '3:00',
+        stars: 4,
+    },
+    {
+        title: 'Quiz 7',
+        description: "Lorem ipsum general default text whatever you like wtf wednesday thursday friday.Lorem ipsum general default text whatever you like wtf wednesday thursday friday.",
+        time: '3:00',
+        stars: 4,
+    },
+]
+
+const announcements = () => {
+    return (
+        <ScrollView style={styles.main}>
+            <View style={styles.controls}>
+                <TouchableOpacity style={{ backgroundColor: '#B29EFF', padding: 8, borderRadius: 10, }} >
+                    <Text style={{color:'#ffffff'}}>
+                        + NEW
+                    </Text>
+                </TouchableOpacity>
+                <View style={styles.picker}>
+                    <Picker
+                        selectedValue={"TIME"}
+                    >
+                        <Picker.Item label="Time" value="Time" />
+                        <Picker.Item label="Item 1" value="Time" />
+                        <Picker.Item label="Item 2" value="Time" />
+                        <Picker.Item label="Item 3" value="Time" />
+                        <Picker.Item label="JavaScript" value="js" />
+                    </Picker>
+                </View>
+            </View>
+
+            {
+                announcementsArray.map((item) => {
+                    return <AnnouncementCard
+                        title={item.title}
+                        key={item.title}
+                        description={item.description}
+                        time={item.time}
+                        stars={item.stars}
+                    />
+                })
+            }
+        </ScrollView>
+    )
+}
+
+export default announcements
+
+const styles = StyleSheet.create({
+    main: {
+        flex: 1,
+        backgroundColor: 'white',
+    },
+    controls: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        padding: 10,
+        marginVertical: 10,
+    },
+    picker: {
+        width: 150,
+        ...Platform.select({
+            ios: {
+              shadowColor: 'gray',
+              shadowOffset: { width: 0, height: 2 },
+              shadowOpacity: 0.2,
+              shadowRadius: 1,
+            },
+            android: {
+              elevation: 1,
+            },
+          }),
+        borderColor: 'gray',
+        borderRadius:0,
+    }
+});
---
+++ components/AnnouncementCard/AnnouncementCard.jsx
@@ -0,0 +1,59 @@
+import { View, Text, StyleSheet, Image,TouchableOpacity } from 'react-native'
+import React from 'react'
+import { color } from 'react-native-reanimated'
+
+const AnnouncementCard = ({ title, description, stars, time }) => {
+    const starsToBeRendered = [];
+    for (let i = 0; i < stars; i++) {
+        starsToBeRendered.push(i);
+    };
+    return (
+        <TouchableOpacity style={styles.cardConatiner}>
+            <View style={styles.starContainer}>
+                {
+                    starsToBeRendered.map((item) => <Image
+                        source={require('../../assets/star.png')}
+                        key={item}
+                        style={styles.star} />)
+                }
+            </View>
+            <Text style={styles.title}>
+                {title}
+            </Text>
+            <Text>{description}</Text>
+            <Text>
+                {time}
+            </Text>
+        </TouchableOpacity>
+    )
+}
+
+export default AnnouncementCard
+
+const styles = StyleSheet.create({
+    cardConatiner: {
+        backgroundColor: '#B29EFF',
+        margin: 10,
+        padding: 10,
+        borderRadius:10
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: "bold",
+        color: '#fff'
+    },
+    description: {
+        fontSize: 12,
+        fontWeight: "normal",
+        color: 'gray'
+    },
+    starContainer: {
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+    },
+    star: {
+        width: 30,   // Adjust the width as needed
+        height: 30,  // Adjust the height as needed
+
+    }
+})
